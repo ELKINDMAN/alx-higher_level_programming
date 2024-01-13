ec2fc1cfbccbc9eebb395b1ecec29dd3e9dfd42d
elkanah <elkanahkindness@gmail.com>
2024-01-13 08:54:18 -0800

    task 0 and task 1 completed!

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+ldig = abs(number) % 10
+if ldig > 5:
+	print(f"Last digit of {number} is {ldig} and is greater than 5")
+elif ldig == 0:
+	print(f"Last digit of {number} is {ldig} and is 0")
+else:
+	print(f"Last digit of {number} is {ldig} and is less than 6 and not 0")
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,5 @@
+0x01. Python - if/else, loops, functions
+Python
+ By: Guillaume
+ Weight: 1
+ Project over - took place from Jan 2, 2024 6:00 AM to Jan 3, 2024 6:00 AM

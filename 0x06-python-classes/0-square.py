d8b234d11f7d9e12f15cdedfc8ccb6e11b794dc3
Kindness Mbaekwe <elkanahkindness@gmail.com>
2024-01-28 07:05:55 +0100

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""This is a documentation for a square module
+    there is no functionality to it
+ """
 class Square:
     '''an empty class Square that defines a square: Square'''
     pass

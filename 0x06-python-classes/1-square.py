5d49b443c5fe390bed3d39b6643ebf60e4235ba2
Kindness Mbaekwe <elkanahkindness@gmail.com>
2024-01-28 07:08:24 +0100

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-
+"""This is a documentation for a square module
+    there is no functionality to it
+ """
 class Square:
-    '''Square: A class that definesa square by; based on 0-square.py
+    '''Square: A class that definesa square by: based on 0-square.py
+    
     private instance attribute: size
     instantiation with size(no type verification)
     '''

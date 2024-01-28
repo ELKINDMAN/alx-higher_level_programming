315e56bb12855977f48f64645ad252786f303f87
elkanah <elkanahkindness@gmail.com>
2024-01-27 21:31:40 -0800

    documention error in task 0 and task 1 fixed!

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 class Square:
+    '''Square: A class that definesa square by; based on 0-square.py
+    private instance attribute: size
+    instantiation with size(no type verification)
+    '''
     def __init__(self, size):
         self.__size = size
 

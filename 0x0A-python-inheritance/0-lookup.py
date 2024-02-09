ee9940e727f3f62c402b1ac7879e6773b7fda99a
elkanah <elkanahkindness@gmail.com>
2024-02-08 22:49:03 -0800

    fixed! doc

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,6 +3,9 @@
 that returns the list of available attributes and method
 '''
 
-
 def lookup(obj):
+    '''Prototype: def lookup(obj):
+    returns a list object
+    You are not allowed to import any module'''
+
     print(dir(obj))
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''
+a simple module with a class: MyList"
+'''
+
+class MyList(List):
+    def print_sorted(self):
+        print(sorted(self))

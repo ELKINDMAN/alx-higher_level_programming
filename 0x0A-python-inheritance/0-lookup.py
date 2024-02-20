b98828cbcb4a0fdf7e80050c134205140a67e342
elkanah <elkanahkindness@gmail.com>
2024-02-20 13:54:35 -0800

    checkedgit add .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,8 +2,8 @@
 '''
 that returns the list of available attributes and method
 '''
-
 def lookup(obj):
+
     '''Prototype: def lookup(obj):
     returns a list object
     You are not allowed to import any module'''

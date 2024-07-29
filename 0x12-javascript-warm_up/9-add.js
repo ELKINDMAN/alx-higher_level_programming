d88d75514279e0570e3b40df44ed1a148c5ea0cb
elkindman <elkanahkindness@gmail.com>
2024-07-29 17:24:04 +0100

    9-add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const cmdVal = process.argv[2];
-const cmdVal2 = process.argv[3];
+const cmdVal = Number(process.argv[2]);
+const cmdVal2 = Number(process.argv[3]);
 
 function add(a, b) {
 	console.log(a + b);

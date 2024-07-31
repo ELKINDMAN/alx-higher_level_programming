5b3a75ccff8b4feec566fb83f514ba9cde1f05e1
elkindman <elkanahkindness@gmail.com>
2024-07-31 18:58:15 +0100

    0. Get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""The module on MySQLdb"""
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    args = sys.argv
+    user = args[1]
+    password = args[2]
+    database = args[3]
+
+    db = MySQLdb.connect(host = 'localhost', user = user,
+                        paswword = pasword, port = 3306, database = database)
+
+    dcur = db.cursor()
+    dbquery = """SELECT * FROM states ORDER BY id ASC"""
+    dcur.execute(dbquery)
+    dbresult = dcur.fetchall()
+    for x in result:
+        print(x)
+
+        dcur.close()
+        db.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,9 @@
+0x0F. Python - Object-relational mapping
+Python
+OOP
+SQL
+MySQL
+ORM
+SQLAlchemy
+ Weight: 1
+ Project over - took place from Jun 20, 2024 6:00 AM to Jun 24, 2024 6:00 AM

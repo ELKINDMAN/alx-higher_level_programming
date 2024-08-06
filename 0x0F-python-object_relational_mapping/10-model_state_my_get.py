60e4ed2709e2dfdbd6716b69cfd3206df241d085
elkindman <elkanahkindness@gmail.com>
2024-08-06 01:13:27 +0100

    task 9

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -7,14 +7,13 @@ from sqlalchemy.orm import Session
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-                    sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-            Base.metadata.create_all(engine)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-                session = Session(bind=engine)
-
-                    result = session.query(State).filter_by(name=sys.argv[4])
-                        if result.all():
-                                    print(result.all()[0].id)
-                                        else:
-                                                    print("Not found")
+    session = Session(bind=engine)
+    result = session.query(State).filter_by(name=sys.argv[4])
+    if result.all():
+        print(result.all()[0].id)
+    else:
+        print("Not found")

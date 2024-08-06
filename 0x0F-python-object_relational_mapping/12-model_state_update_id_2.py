a512a176261761b7ab0fa02c21021d18cd5b7de6
elkindman <elkanahkindness@gmail.com>
2024-08-06 05:19:00 +0100

    12 model

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -3,17 +3,27 @@
 """
 import sys
 from model_state import Base, State
-from sqlalchemy.orm import Session
-from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-            .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(bind=engine)
+def main():
+    """update value in database
+    """
+    aval = sys.argv[1]
+    bval = sys.argv[2]
+    cval = sys.argv[3]
 
-    session = Session(bind=engine)
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'
+            .format(aval, bval, cval), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter(State.id == 2).first()
 
-    result = session.query(State).get(2)
-    result.name = 'New Mexico'
+    state.name = 'New Mexico'
+
     session.commit()
     session.close()
+
+    if __name__ == "__main__":
+        main()
